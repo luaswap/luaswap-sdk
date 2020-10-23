5a8df09881ea8c6ad717451b53ca333a48c4a3e7
PhucNH <phucnh.dev@gmail.com>
2020-10-23 11:51:21 +0700

    Change uniswap factory address to luaswap factory address

--- src/constants.ts
+++ src/constants.ts
@@ -23,7 +23,7 @@ export enum Rounding {
 }
 
 // TODO: Need to change to luaswap's factory address
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0x0160835743D9bbd788321dfEff9f58F231677d6A'
 
 export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
 
